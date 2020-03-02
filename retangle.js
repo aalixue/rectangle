1ce63bc94eb4bba67065974c0217bbd72235a5b2
aalixue <2319684501@qq.com>
2020-03-02 17:53:20 +0800

    .

--- retangle.js
+++ retangle.js
@@ -1,31 +1,23 @@
+/* global $: true */
 $(function(){
-    //get dom elem
-    var $width = $('#width'),
-        $height = $('#height'),
-        $button = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
-    /*calc button click event */
-    $button.click(function(){
-        //get value
-        var w = Number($width.val()),
-            h = Number($height.val());
-        console.log(w,h);
-        //calculate
-        var p = 2 * (w + h);
-        p = Math.round(p * Math.pow(10,2)) / Math.pow(10,2);
-        var a = w * h;
-        a = Math.round(a * Math.pow(10,2)) / Math.pow(10,2);
-        //output
-        $perimeter.val(p);
-        $area.val(a);
-    })
-    /**
-     * //get value
-
-        //calculate
-
-        //output
-     * 
-     */
-})
+  //get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $button = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  /*calc button click event */
+  $button.click(function(){
+    //get value
+    var w = Number($width.val()),
+        h = Number($height.val());
+    //calculate
+    var p = 2 * (w + h);
+    p = Math.round(p * Math.pow(10,2)) / Math.pow(10,2);
+    var a = w * h;
+    a = Math.round(a * Math.pow(10,2)) / Math.pow(10,2);
+    //output
+    $perimeter.val(p);
+    $area.val(a);
+  });
+});
